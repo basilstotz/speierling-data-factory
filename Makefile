6c963a9a0d0636b6b0e049abc34bb09fdebcc072
Basil Stotz <stotz.basil@opinsys.fi>
2025-02-09 20:06:46 +0100

    refactoring history

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .PHONY: all
 all: sorbusdomestica.geojson
 
-sorbusdomestica.geojson: osm/sorbusdomestica.geojson tmp/project.json bilder
+sorbusdomestica.geojson: osm/sorbusdomestica.geojson tmp/project.json   #bilder 
 	./bin/update-geojson.sh
 
 #tmp/bilder.json: node  
@@ -11,17 +11,20 @@ tmp/project.json: projekt.csv
 	test -d ./tmp || mkdir -p ./tmp
 	csv2json -d ./projekt.csv ./tmp/project.json 
 
+osm/sorbusdomestica.geojson:
+	$(MAKE) -C ./osm update
+
+
+
 .PHONY: bilder
 bilder:
 	@test -d ./../../Gemeinden || (echo "./../../Gemeinden not found";false)
 	./bin/convert-all.sh ./../../Gemeinden ./../../node
 	./bin/make-bilder.js ./../../node/
 
+
 .PHONY: archive
 archive:
 	test -d archive || mkdir -p archive
 	./bin/archive.sh	
 
-########################################################################################
-osm/sorbusdomestica.geojson:
-	$(MAKE) -C ./osm all
--- bin/update-geojson.sh
+++ bin/update-geojson.sh
@@ -4,7 +4,7 @@ cd "$(dirname $0)/../."
 
 pwd
 
-./bin/update-history.sh >> ./update-history/update-history.log
+#./bin/update-history.sh >> ./update-history/update-history.log
 
 #cp ./../../node/mediaIndex.json mediaIndex.json
 curl https://speierling.arglos.ch/node/mediaIndex.json 2>/dev/null  >mediaIndex.json
@@ -15,11 +15,11 @@ cat ./osm/sorbusdomestica.geojson | \
             ./bin/add-media.js mediaIndex.json | \
             ./bin/process-project.js ./tmp/project.json 2> project.log | \
             ./bin/add-historic.js   | \
-            ./bin/process-history.js ./update-history/history.geojson  |  \
+            ./bin/process-history.js   |  \
 	    ./bin/add-growth.js > sorbusdomestica.geojson
               
 
-cat sorbusdomestica.geojson | ./bin/devel/reduce.js | ./bin/devel/flatten-tags.js > ./../../sorbusdomestica.geojson
+#cat sorbusdomestica.geojson | ./bin/devel/reduce.js | ./bin/devel/flatten-tags.js > ./../../sorbusdomestica.geojson
 
 
 echo "done"
