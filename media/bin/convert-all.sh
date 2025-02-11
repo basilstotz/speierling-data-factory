e3a5d9fe10efd2bca4b292e499705412220ec184
Basil Stotz <stotz.basil@opinsys.fi>
2025-02-11 16:52:46 +0100

    handle empty dirs

--- media/bin/convert-all.sh
+++ media/bin/convert-all.sh
@@ -5,10 +5,9 @@ DST_DIR=$2
 
 DIRS=$(find $SRC_DIR -type d  |grep  ".*[^.]\.[0-9]*$")
 
-
 for DIR in $DIRS; do
 	ID="$(basename $DIR|cut -d. -f2)"
-	for FILE in $DIR/*; do
+	for FILE in $(find $DIR -mindepth 1 -maxdepth 1 -type f); do
 		BASENAME="$(basename $FILE)"
 		if ! test -f "$DST_DIR/$ID/$BASENAME";then
 			echo "add $FILE"
@@ -17,10 +16,6 @@ for DIR in $DIRS; do
                         if test -z "$TIME"; then
                           TIME=$(exiftool -s -s -s -d "%e.%B %Y" -FileAccessDate  $FILE)
                         fi
-			# echo $TIME
-                        #test -z "$TIME" || TIME=" "
-			#        		convert -resize 1024x1024  "$FILE" "$DST_DIR/$ID/$BASENAME"
-			#			convert -resize 1024x1024 -pointsize 20 -fill yellow -draw "text 100,100 $TIME"  "$FILE" "$DST_DIR/$ID/$BASENAME"
 			convert  -auto-orient -resize 1024x1024 -pointsize 40 -fill yellow -gravity SouthEast  -annotate 0  "$TIME" "$FILE" "$DST_DIR/$ID/$BASENAME" 
 		fi
 		if ! test -f "$DST_DIR/$ID/thumbs/$BASENAME";then
@@ -29,7 +24,6 @@ for DIR in $DIRS; do
 		fi
 	done
 done
-
 
 exit
 
